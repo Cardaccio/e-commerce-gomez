24d578fd21baa228d34cdee39a4584bb2e9ad86f
Cardaccio <juangomezpablo@gmail.com>
2022-08-24 17:11:52 -0300

    ItemCount component

---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import Button from '@mui/material/Button';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+
+function ItemCount({ stock, initial, onAdd }) {
+  const [cant, setCant] = useState(initial);
+  return (
+    <div className="item-div">
+      <h3>Producto</h3>
+      <div className="item-qty">
+        <Button style={{ fontSize: '2rem  ' }} variant="text" 
+          onClick={() => {
+            if (cant > 0) {
+              setCant(cant - 1);
+            }
+          }}
+        >
+          -
+        </Button>
+        <p>{cant}</p>
+        <Button style={{ fontSize: '2rem  ' }} variant="text"
+          onClick={() => {
+            if (cant < stock) {
+              setCant(cant + 1);
+            }
+          }}
+        >
+          +
+        </Button>
+      </div>
+      <Button variant="outlined"
+        onClick={() => {
+          if (cant > 0) {
+            onAdd(cant);
+          }
+        }}
+      >
+        <AddShoppingCartIcon />  Agregar al Carrito
+      </Button>
+    </div>
+  );
+}
+
+export default ItemCount;
