53ac8bed7b725ffbccac07235d6340c0f9c2c460
Cardaccio <juangomezpablo@gmail.com>
2022-09-07 15:27:49 -0300

    Readme File

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ItemListContainer from "./components/ItemListContainer";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import ItemDetailContainer from "./components/ItemDetailContainer";
+import Evento from "./test/Evento";
 
 
 function App() {
@@ -28,6 +29,7 @@ function App() {
         />
           <Routes>
             <Route path="/" element={<ItemListContainer className="center"/>}/>
+            <Route path="/test" element={<Evento className="center"/>}/>
             <Route path="/category/:idcategory" element={<ItemListContainer className="center"/>}/>
             <Route path="/product/:idproduct" element={<ItemDetailContainer className="center"/>}/>
           </Routes>
